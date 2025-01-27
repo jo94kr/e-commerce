caaf551178205c061830cd6d4d3323c527b7e350
조진우 <wls94kr@forbiz.co.kr>
2025-01-27 13:08:13 +0900

    init

---
+++ src/test/java/com/jinu/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jinu.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
